69927628279e2912771e1b716d4ecafcbfd6168d
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-04-25 12:07:25 +0200

    feat: :sparkles: Add Shadcn
    Add Shadcn components

--- frontend/info-skjerm/components/ui/NavBar.tsx
+++ frontend/info-skjerm/components/ui/NavBar.tsx
@@ -4,11 +4,18 @@ export function NavBar() {
   return (
     <main>
       <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
+<<<<<<< HEAD
         <Link href="/infoskjerm">
           <div className="flex items-center space-x-4">
             <h1 className="text-2xl font-bold">Infoskjerm</h1>
           </div>
         </Link>
+=======
+        <div className="flex items-center space-x-4">
+          {/* <img src="/logo.png" alt="Logo" className="w-12 h-12" /> */}
+          <h1 className="text-2xl font-bold">Info-skjerm</h1>
+        </div>
+>>>>>>> 830e451 (feat: :sparkles: Add Shadcn)
         <div className="flex items-center space-x-4">
           <a href="/" className="hover:underline">
             Hjem
@@ -16,6 +23,7 @@ export function NavBar() {
           <a href="/admin" className="hover:underline">
             Admin
           </a>
+<<<<<<< HEAD
           <a href="/auth/signin" className="hover:underline">
             Logg inn
           </a>
@@ -23,6 +31,8 @@ export function NavBar() {
             Logg ut
           </a>
           {/* <Button onClick={() => handleSignOut()}>Logg ut</Button> */}
+=======
+>>>>>>> 830e451 (feat: :sparkles: Add Shadcn)
         </div>
       </nav>
     </main>
