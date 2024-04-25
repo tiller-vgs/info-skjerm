830e4513e8f0869d253940356a8a5507bf727d49
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-04-25 12:07:25 +0200

    feat: :sparkles: Add Shadcn
    Add Shadcn components

---
+++ frontend/info-skjerm/components/ui/NavBar.tsx
@@ -0,0 +1,20 @@
+export function NavBar() {
+  return (
+    <main>
+      <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
+        <div className="flex items-center space-x-4">
+          {/* <img src="/logo.png" alt="Logo" className="w-12 h-12" /> */}
+          <h1 className="text-2xl font-bold">Info-skjerm</h1>
+        </div>
+        <div className="flex items-center space-x-4">
+          <a href="/" className="hover:underline">
+            Hjem
+          </a>
+          <a href="/admin" className="hover:underline">
+            Admin
+          </a>
+        </div>
+      </nav>
+    </main>
+  );
+}
