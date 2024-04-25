c2f26ac8a4fe9adaa9c0ce0e8eace12e4381155c
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-04-25 11:56:35 +0200

    feat: :sparkles: Add Shadcn
    Add Shadcn components

--- frontend/info-skjerm/components/ui/NavBar.tsx
+++ frontend/info-skjerm/components/ui/NavBar.tsx
@@ -1,16 +1,9 @@
-// "use client";
-import { signOut } from "@/auth";
-import { Button } from "./button";
-
-const handleSignOut = async () => {
-  return await signOut();
-};
-
 export function NavBar() {
   return (
     <main>
       <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
         <div className="flex items-center space-x-4">
+          {/* <img src="/logo.png" alt="Logo" className="w-12 h-12" /> */}
           <h1 className="text-2xl font-bold">Info-skjerm</h1>
         </div>
         <div className="flex items-center space-x-4">
@@ -20,13 +13,6 @@ export function NavBar() {
           <a href="/admin" className="hover:underline">
             Admin
           </a>
-          <a href="/auth/signin" className="hover:underline">
-            Logg inn
-          </a>
-          <a href="/auth/signout" className="hover:underline">
-            Logg ut
-          </a>
-          {/* <Button onClick={() => handleSignOut()}>Logg ut</Button> */}
         </div>
       </nav>
     </main>
