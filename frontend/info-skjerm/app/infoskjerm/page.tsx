450fc6616655430dcf49e12faf683818eef6c449
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-04-26 09:51:29 +0200

    WIP
    Refs: Code infoskjerm #16

--- frontend/info-skjerm/app/infoskjerm/page.tsx
+++ frontend/info-skjerm/app/infoskjerm/page.tsx
@@ -1,9 +1,55 @@
+"use client";
 import React from "react";
+import Weather from "./Weather";
+import DateTime from "./DateTime";
+import Bus from "./Bus";
 
 export default function InfoPage() {
   return (
-    <main>
-      <h1>Her er infoskjermen</h1>
+    <main className=" p-3">
+      <div className=" flex gap-3">
+        <DateTime />
+        <Weather />
+        {/* <Bus /> */}
+      </div>
+      <div className="pt-3">
+        <div className="border-2 border-white rounded-lg w-96 h-auto max-h-screen overflow-hidden">
+          <h1 className="text-2xl p-3 bg-white bg-opacity-10">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
+            elit tempor, tempor nunc eget, ultricies dui. Phasellus ut finibus
+            velit. Maecenas sit amet dolor tortor. Fusce eleifend, quam id
+            condimentum porttitor, eros eros sagittis libero, vitae lobortis
+            magna nisi sit amet ipsum. Curabitur cursus, diam et volutpat
+            iaculis, nibh magna aliquet sem, sit amet aliquam tellus lacus eu
+            metus. Nunc rutrum nulla vel mauris sodales, vitae sollicitudin
+            turpis rhoncus. Nullam nec nunc nec nunc ultricies ultricies.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
+            elit tempor, tempor nunc eget, ultricies dui. Phasellus ut finibus
+            velit. Maecenas sit amet dolor tortor. Fusce eleifend, quam id
+            condimentum porttitor, eros eros sagittis libero, vitae lobortis
+            magna nisi sit amet ipsum. Curabitur cursus, diam et volutpat
+            iaculis, nibh magna aliquet sem, sit amet aliquam tellus lacus eu
+            metus. Nunc rutrum nulla vel mauris sodales, vitae sollicitudin
+            turpis rhoncus. Nullam nec nunc nec nunc ultricies ultricies.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
+            elit tempor, tempor nunc eget, ultricies dui. Phasellus ut finibus
+            velit. Maecenas sit amet dolor tortor. Fusce eleifend, quam id
+            condimentum porttitor, eros eros sagittis libero, vitae lobortis
+            magna nisi sit amet ipsum. Curabitur cursus, diam et volutpat
+            iaculis, nibh magna aliquet sem, sit amet aliquam tellus lacus eu
+            metus. Nunc rutrum nulla vel mauris sodales, vitae sollicitudin
+            turpis rhoncus. Nullam nec nunc nec nunc ultricies ultricies.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
+            elit tempor, tempor nunc eget, ultricies dui. Phasellus ut finibus
+            velit. Maecenas sit amet dolor tortor. Fusce eleifend, quam id
+            condimentum porttitor, eros eros sagittis libero, vitae lobortis
+            magna nisi sit amet ipsum. Curabitur cursus, diam et volutpat
+            iaculis, nibh magna aliquet sem, sit amet aliquam tellus lacus eu
+            metus. Nunc rutrum nulla vel mauris sodales, vitae sollicitudin
+            turpis rhoncus. Nullam nec nunc nec nunc ultricies ultricies.
+          </h1>
+        </div>
+      </div>
     </main>
   );
 }
