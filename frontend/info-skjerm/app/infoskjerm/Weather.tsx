f5b6cb624ecee46c53feff8174e05f36669a0a88
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-04-26 09:51:29 +0200

    WIP
    Refs: Code infoskjerm #16

---
+++ frontend/info-skjerm/app/infoskjerm/Weather.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="border-2 border-white  rounded-lg w-screen h-20">
+      <div>
+        <h1 className="text-2xl font-bold text-center p-1">Værmelding</h1>
+      </div>
+      <div>
+        <p className="text-center">Været er fint</p>
+      </div>
+    </div>
+  );
+}
