65c510761dc3570d61d0fb1f2b3f1a448330200d
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-04-26 11:17:01 +0200

    Add weather symbols
    Add weather symbols and add some dummydata to display
    
    Refs: Code infoskjerm #16

--- frontend/info-skjerm/app/infoskjerm/page.tsx
+++ frontend/info-skjerm/app/infoskjerm/page.tsx
@@ -6,13 +6,12 @@ import Bus from "./Bus";
 
 export default function InfoPage() {
   return (
-    <main className=" p-3">
-      <div className=" flex gap-3">
+    <main className=" p-3 max-w-screen">
+      <div className=" pb-3 flex gap-3">
         <DateTime />
         <Weather />
-        {/* <Bus /> */}
       </div>
-      <div className="pt-3">
+      <div className="pt-3 w-screen flex">
         <div className="border-2 border-white rounded-lg w-96 h-auto max-h-screen overflow-hidden">
           <h1 className="text-2xl p-3 bg-white bg-opacity-10">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
@@ -21,33 +20,11 @@ export default function InfoPage() {
             condimentum porttitor, eros eros sagittis libero, vitae lobortis
             magna nisi sit amet ipsum. Curabitur cursus, diam et volutpat
             iaculis, nibh magna aliquet sem, sit amet aliquam tellus lacus eu
-            metus. Nunc rutrum nulla vel mauris sodales, vitae sollicitudin
-            turpis rhoncus. Nullam nec nunc nec nunc ultricies ultricies.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
-            elit tempor, tempor nunc eget, ultricies dui. Phasellus ut finibus
-            velit. Maecenas sit amet dolor tortor. Fusce eleifend, quam id
-            condimentum porttitor, eros eros sagittis libero, vitae lobortis
-            magna nisi sit amet ipsum. Curabitur cursus, diam et volutpat
-            iaculis, nibh magna aliquet sem, sit amet aliquam tellus lacus eu
-            metus. Nunc rutrum nulla vel mauris sodales, vitae sollicitudin
-            turpis rhoncus. Nullam nec nunc nec nunc ultricies ultricies.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
-            elit tempor, tempor nunc eget, ultricies dui. Phasellus ut finibus
-            velit. Maecenas sit amet dolor tortor. Fusce eleifend, quam id
-            condimentum porttitor, eros eros sagittis libero, vitae lobortis
-            magna nisi sit amet ipsum. Curabitur cursus, diam et volutpat
-            iaculis, nibh magna aliquet sem, sit amet aliquam tellus lacus eu
-            metus. Nunc rutrum nulla vel mauris sodales, vitae sollicitudin
-            turpis rhoncus. Nullam nec nunc nec nunc ultricies ultricies.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu
-            elit tempor, tempor nunc eget, ultricies dui. Phasellus ut finibus
-            velit. Maecenas sit amet dolor tortor. Fusce eleifend, quam id
-            condimentum porttitor, eros eros sagittis libero, vitae lobortis
-            magna nisi sit amet ipsum. Curabitur cursus, diam et volutpat
-            iaculis, nibh magna aliquet sem, sit amet aliquam tellus lacus eu
-            metus. Nunc rutrum nulla vel mauris sodales, vitae sollicitudin
-            turpis rhoncus. Nullam nec nunc nec nunc ultricies ultricies.
+            metus.
           </h1>
+        </div>
+        <div className=" absolute right-0 pr-3">
+          <Bus />
         </div>
       </div>
     </main>
