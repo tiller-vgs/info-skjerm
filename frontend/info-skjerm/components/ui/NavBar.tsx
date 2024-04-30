7cb7fed48fd411a7d2922ecc71cb82efe1ac8291
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-04-30 11:01:18 +0200

    WIP

--- frontend/info-skjerm/components/ui/NavBar.tsx
+++ frontend/info-skjerm/components/ui/NavBar.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
+
 export function NavBar() {
   return (
     <main>
       <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
-        <div className="flex items-center space-x-4">
-          {/* <img src="/logo.png" alt="Logo" className="w-12 h-12" /> */}
-          <h1 className="text-2xl font-bold">Info-skjerm</h1>
-        </div>
+        <Link href="/infoskjerm">
+          <div className="flex items-center space-x-4">
+            <h1 className="text-2xl font-bold">Infoskjerm</h1>
+          </div>
+        </Link>
         <div className="flex items-center space-x-4">
           <a href="/" className="hover:underline">
             Hjem
