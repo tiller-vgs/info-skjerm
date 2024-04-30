f0628d94d22e8428bb90d1782d42ff09aef171c3
Marcus-Aastum <marcusaastum@gmail.com>
2024-04-30 14:11:09 +0200

    Create SQL init file
    Create SQL file that creates tables with collumns
    
    Create database init file #14

--- Database/mssql/scripts/init.sql
+++ Database/mssql/scripts/init.sql
@@ -1,2 +1,20 @@
--- This file is not finished, and mainly exists for test and example reasons
 CREATE DATABASE Info_Skjerm;
+USE Info_Skjerm;
+
+CREATE TABLE Users
+(
+    email VARCHAR(100) PRIMARY KEY,
+    fullname VARCHAR(100),
+    password_hash TEXT,
+);
+
+CREATE TABLE Events
+(
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    title VARCHAR(100),
+    body TEXT,
+    startdate DATE,
+    starttime TIME,
+    enddate DATE,
+    endtime TIME,
+);
