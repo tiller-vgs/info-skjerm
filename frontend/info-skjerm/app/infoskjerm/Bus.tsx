f239ebbf8e9436c2332780f7a4b4453c5359ce76
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-04-30 11:01:18 +0200

    WIP

--- frontend/info-skjerm/app/infoskjerm/Bus.tsx
+++ frontend/info-skjerm/app/infoskjerm/Bus.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Bus() {
   return (
-    <div className="border-2 border-white rounded-lg w-72 h-96">
+    <div className="border-2 border-white rounded-lg w-96 h-96">
       <div>
         <h1 className="text-2xl font-bold text-center p-1">Busser</h1>
       </div>
