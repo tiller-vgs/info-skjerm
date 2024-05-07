08f251d0faf8d8a08055c113c1ac5045caf5621b
Hexcrow-k17 <kimaleksander1702@gmail.com>
2024-05-07 13:22:43 +0200

    WIP

--- frontend/info-skjerm/app/infoskjerm/Bus.tsx
+++ frontend/info-skjerm/app/infoskjerm/Bus.tsx
@@ -2,9 +2,30 @@ import React from "react";
 
 export default function Bus() {
   return (
-    <div className="border-2 border-slate-500 rounded-lg h-60">
+    <div className="border-2 border-slate-500 rounded-lg h-auto pb-2">
       <div>
         <h1 className="text-2xl font-bold text-center p-1">Busser</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        <h1 className="text-2xl font-bold text-center p-1">jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
       </div>
     </div>
   );
--- frontend/info-skjerm/app/infoskjerm/page.tsx
+++ frontend/info-skjerm/app/infoskjerm/page.tsx
@@ -9,21 +9,21 @@ export default function InfoPage() {
   return (
     <main className="p-3">
       <div className="grid grid-rows-3 grid-flow-col gap-4">
-        <div className="row-span-3">
+        <div className="row-span-2">
           <DateTime />
         </div>
-        <div className="col-span-2">
+        <div className="row-span-2">
           <WeatherHours />
         </div>
-        <div className="col-span-2">
+        <div className="row-span-3 col-span-1">
           <WeatherDays />
+        </div>
+        <div className="col-span-1">
+          <Info />
         </div>
         <div className="row-span-3">
           <Bus />
         </div>
-      </div>
-      <div className="">
-        <Info />
       </div>
     </main>
   );
