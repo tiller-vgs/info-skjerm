a0163cee44bbbc04251c5cbf5c29c22616f723c8
Marcus-Aastum <marcusaastum@gmail.com>
2024-05-13 10:31:23 +0200

    Documentation, week events and gitignore
    Add better documentation to GetEvents-controller. Fix weeksevent-endpoint to start on monday instead of sunday. Add appsettings to gitignore.
    
    Create get endpoints on backend (Events) #20

--- Backend/info-skjerm-api/Controllers/GetEventsController.cs
+++ Backend/info-skjerm-api/Controllers/GetEventsController.cs
@@ -1,20 +1,27 @@
-using System.ComponentModel;
-using System.Text.Json;
+/*
+This file is for all GET-endpoints for Events
+For other endpoints, create new controllers
+The route for these endpoints are {baseurl}/GetEvents/{endpoint}
+
+Authored by @Marcus-Aastum
+*/
+
 using info_skjerm_api.Model;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace info_skjerm_api.Controllers
 {
-
     [ApiController]
     [Route("[controller]")]
     public class GetEventsController : ControllerBase
     {
+        //Defines context for database
         private readonly ApplicationDbContext _context;
         public GetEventsController(ApplicationDbContext context) {
             _context = context;
         }
+
+        //This endpoint returns all events in the database
         [HttpGet("allevents")]
         public IActionResult GetAllEvents()
         {
@@ -22,6 +29,7 @@ namespace info_skjerm_api.Controllers
             return Ok(events);
         }
         
+        //This endpoint gets a specific event by an ID
         [HttpGet("eventbyid/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -31,6 +39,7 @@ namespace info_skjerm_api.Controllers
             return (eventitem != null) ? Ok(eventitem) : NotFound();
         }
 
+        //This endpoint returns all events happening this day
         [HttpGet("todaysevents")]
         public IActionResult GetEventsToday() {
             List<Events> events = _context.Events.ToList();
@@ -48,34 +57,42 @@ namespace info_skjerm_api.Controllers
             return Ok(eventstoday);
         }
 
+        //This endpoint returns all events happening in the current calendar week
         [HttpGet("weeksevents")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetWeeksEvents(){
 
-            //TODO: This currently uses a week that starts on sunday, change this to monday
+            //Function to check if dates are in this week (use date1 for current date, date2 for event date)
             bool DatesAreInTheSameWeek(DateTime date1, DateTime date2)
             {
+                var fixdate2 = date2.Date.AddDays(-1);
                 var cal = System.Globalization.DateTimeFormatInfo.CurrentInfo.Calendar;
                 var d1 = date1.Date.AddDays(-1 * (int)cal.GetDayOfWeek(date1));
-                var d2 = date2.Date.AddDays(-1 * (int)cal.GetDayOfWeek(date2));
+                var d2 = fixdate2.Date.AddDays(-1 * (int)cal.GetDayOfWeek(fixdate2));
 
-                return d1 == d2;
+                return d1.Date == d2.Date;
             }
             List<Events> events = _context.Events.ToList();
             List<Events> weeksevents = [];
             for (int i = 0; i < events.Count; i++)
             {
+                //If starttime is in this week
                 if (events[i].starttime != null && DatesAreInTheSameWeek(DateTime.Now, (DateTime)events[i].starttime)){
                     weeksevents.Add(events[i]);
                 }
+                //if endtime is in this week
                 else if (events[i].endtime != null && DatesAreInTheSameWeek(DateTime.Now, (DateTime)events[i].endtime)){
+                    weeksevents.Add(events[i]);
+                }
+                //If both starttime is before this week, and endtime is after this week, meaning the event is still happening
+                else if(events[i].starttime < DateTime.Now && events[i].endtime > DateTime.Now){
                     weeksevents.Add(events[i]);
                 }
                 
             }
 
-            return (weeksevents != null) ? Ok(weeksevents) : NotFound("No events in this week");
+            return (weeksevents.Count != 0) ? Ok(weeksevents) : NotFound("No events in this week");
         }
 
         //DO NOT add POST- PATCH/PUT or DELETE endpoints in this file. Instead, create another conntroller called i.e. "PostEventsController.cs" 
