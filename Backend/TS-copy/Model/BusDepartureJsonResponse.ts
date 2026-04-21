c0d996830ee333b7e6c1dfb31014a285a8a86b78
EmilWollf <emil.r.wollf@gmail.com>
2026-04-21 12:25:18 +0200

    FEAT: set up types for typescript

---
+++ Backend/TS-copy/Model/BusDepartureJsonResponse.ts
@@ -0,0 +1,12 @@
+export interface BusRoute {
+  busLine: number;
+  destination: string;
+  time: string;
+  isRealTime: boolean;
+}
+
+export interface BusStop {
+  northBound: BusRoute[];
+  southBound: BusRoute[];
+  all: BusRoute[];
+}
---
+++ Backend/TS-copy/Model/Event.ts
@@ -0,0 +1,7 @@
+export interface Events {
+  iD: number;
+  title: string | null;
+  body: string | null;
+  starttime: string | null;
+  endtime: string | null;
+}
