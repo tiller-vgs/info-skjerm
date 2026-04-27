9c2535b33882218bc60839326a2d9ba9996d50b8
ma6692 <140921409+ma6692@users.noreply.github.com>
2026-04-27 09:27:53 +0200

    refactor: refactor React routing
    refactor React routing so that app is no longer neaded
    
    refs: #91

--- frontend/info-skjerm-2026/src/main.tsx
+++ frontend/info-skjerm-2026/src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import "@fontsource-variable/inter";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Infoskjerm from "./pages/Infoskjerm";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Infoskjerm />} />
+          <Route path="admin"></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
