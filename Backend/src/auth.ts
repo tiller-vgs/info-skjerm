99ddfc0465fcd44a65eaad3be80656cf1d57ccd6
EmilWollf <emil.r.wollf@gmail.com>
2026-04-28 13:20:32 +0200

    fix

--- Backend/src/auth.ts
+++ Backend/src/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import { prisma } from "./lib/prisma";
+import { prisma } from "./lib/prisma.ts";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
