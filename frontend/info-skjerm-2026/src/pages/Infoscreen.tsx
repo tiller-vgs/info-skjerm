a5a89e7ed389c9f9803f29cd7d00d0a68c9ddc45
OT44-0 <ole.tobias.solberg@icloud.com>
2026-05-04 10:06:13 +0200

    feat: Add buss icon to infoscreen
    refs: #77

--- frontend/info-skjerm-2026/src/pages/Infoscreen.tsx
+++ frontend/info-skjerm-2026/src/pages/Infoscreen.tsx
@@ -2,6 +2,7 @@ import Bus from "../components/Bus";
 import Clock from "../components/Clock";
 import DateDisplay from "../components/DateDisplay";
 import WeatherDisplay from "../components/WeatherDisplay";
+import busIcon from "../Icon/bus.png";
 
 function InfoScreen() {
   return (
@@ -20,7 +21,11 @@ function InfoScreen() {
           </table>
           <WeatherDisplay />
         </td>
-        <td rowSpan={2} className="border-l-10 w-1/4 border-[#21252b]">
+        <td
+          rowSpan={2}
+          className="border-l-10 w-1/4 border-[#21252b] align-top text-center p-4"
+        >
+          <img src={busIcon} alt="bus" width={100} className="inline-block" />
           <Bus />
         </td>
       </tr>
