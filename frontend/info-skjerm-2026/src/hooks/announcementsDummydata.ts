c0f87e68b1d65fda49cbe7f60fa3be9839b76467
ma6692 <140921409+ma6692@users.noreply.github.com>
2026-05-12 18:02:21 +0200

    feat: add dummy data and lock tanstack qerry version
    locked tanstack qerry version to one with no malware
    
    refs: #143

---
+++ frontend/info-skjerm-2026/src/hooks/announcementsDummydata.ts
@@ -0,0 +1,32 @@
+export const useAnnouncements = () => {
+  return [
+    {
+      id: 1,
+      title: "Test melding",
+      content: "3D printer kurs starter i morgen kl. 14:00.",
+      dateStart: "2023-10-01",
+      dateEnd: "2023-10-02",
+    },
+    {
+      id: 2,
+      title: "Test melding 2",
+      content: "Resin printer kurs starter i morgen kl. 14:00.",
+      dateStart: "2023-10-01",
+      dateEnd: "2023-10-02",
+    },
+    {
+      id: 3,
+      title: "Test melding 3",
+      content: "Laser cutter kurs starter i morgen kl. 14:00.",
+      dateStart: "2023-10-01",
+      dateEnd: "2023-10-02",
+    },
+    {
+      id: 4,
+      title: "Test melding 4",
+      content: "CNC kurs starter i morgen kl. 14:00.",
+      dateStart: "2023-10-01",
+      dateEnd: "2023-10-02",
+    },
+  ];
+};
