f9d6baca571b9bc267b289b8f316a5e27063ad6b
Sondre <sondrelb@outlook.com>
2026-05-12 12:12:47 +0200

    npm i

--- frontend/info-skjerm-2026/src/pages/Infoscreen.tsx
+++ frontend/info-skjerm-2026/src/pages/Infoscreen.tsx
@@ -1,10 +1,11 @@
 // import Bus from "../components/Bus";
 // import Clock from "../components/Clock";
 import { BusRotator, AnnouncmentRotator } from "../components/ContentRotator";
-// import DateDisplay from "../components/DateDisplay";
-import WeatherDisplay from "../components/WeatherDisplay";
+
+// import WeatherDisplay from "../components/WeatherDisplay";
 import busIcon from "../Icon/bus.png";
 import DateAndTimeDisplay from "../components/DateAndTimeDisplay";
+import Paper from "@mui/material/Paper";
 
 function InfoScreen() {
   return (
@@ -13,16 +14,17 @@ function InfoScreen() {
         {/* Weather & Time */}
         <td className="bg-tqboxes border-b-10 border-l-10 h-1/4 border-tqbackground">
           <DateAndTimeDisplay />
-          <WeatherDisplay />
+          <p>Weather</p>
+          {/* <WeatherDisplay /> */}
         </td>
         {/* Busses */}
-        <td
-          rowSpan={2}
+        <Paper
+          // rowSpan={2}
           className="border-l-10 w-1/4 border-tqbackground align-top text-center p-4"
         >
           <img src={busIcon} alt="bus" width={150} className="inline-block" />
           <BusRotator />
-        </td>
+        </Paper>
       </tr>
       {/* TillerQuest & Announcements */}
       <tr>
@@ -40,39 +42,38 @@ export default InfoScreen;
 // // import Bus from "../components/Bus";
 // // import Clock from "../components/Clock";
 // import { BusRotator, AnnouncmentRotator } from "../components/ContentRotator";
-// // import DateDisplay from "../components/DateDisplay";
-// import WeatherDisplay from "../components/WeatherDisplay";
+
+// // import WeatherDisplay from "../components/WeatherDisplay";
 // import busIcon from "../Icon/bus.png";
 // import DateAndTimeDisplay from "../components/DateAndTimeDisplay";
 // import Paper from "@mui/material/Paper";
 
 // function InfoScreen() {
 //   return (
-//     <Paper className="flex flex-row bg-tqboxes w-screen h-screen ">
-//       <div className="flex flex-col w-3/4">
+//     <div className="bg-tqboxes border-10 w-screen h-screen border-tqbackground">
+//       <div>
 //         {/* Weather & Time */}
-//         <div className="bg-tqboxes h-1/4 ">
+//         <Paper className="bg-tqboxes border-b-10 border-l-10 h-1/4 border-tqbackground">
 //           <DateAndTimeDisplay />
-//           <WeatherDisplay />
-//         </div>
-
-//         {/* TillerQuest & Announcements */}
-//         <div>
-//           {/* <td className="border-t-10 border-l-10 border-tqbackground"> */}
+//           <p>Weather</p>
+//           {/* <WeatherDisplay /> */}
+//         </Paper>
+//         {/* Busses */}
+//         <Paper
+         
+//           className="border-l-10 w-1/4 border-tqbackground align-top text-center p-4"
+//         >
+//           <img src={busIcon} alt="bus" width={150} className="inline-block" />
+//           <BusRotator />
+//         </Paper>
+//       </div>
+//       {/* TillerQuest & Announcements */}
+//       <Paper>
+//         <td className="border-t-10 border-l-10 border-tqbackground">
 //           <AnnouncmentRotator />
-//           {/* </td> */}
-//         </div>
-//       </div>
-
-//       {/* Busses */}
-//       <div
-//         // rowSpan={2}
-//         className=" w-1/4 border-tqbackground align-top text-center p-4"
-//       >
-//         {/* <img src={busIcon} alt="bus" width={150} className="inline-block" /> */}
-//         <BusRotator />
-//       </div>
-//     </Paper>
+//         </td>
+//       </Paper>
+//     </div>
 //   );
 // }
 
